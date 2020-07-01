ae49b70ce74765927369fefeec454d25781525cb
VladYurash <vlad.yurash@gmail.com>
2020-07-01 23:20:28 +0300

    btn component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link
+    class="btn"
+    :to="to"
+    :class="[
+      themes[theme], sizes[size]
+    ]"
+  >
+    Sign in
+  </router-link>
+</template>
+
+<script>
+  export default {
+    name: 'AppButton',
+    data: () => ({
+      themes: {
+        primary: 'btn-primary',
+        danger: 'btn-danger'
+      },
+      sizes: {
+        normal: '',
+        large: 'btn-lg'
+      }
+    }),
+    props: {
+      theme: {
+        required: false,
+        type: String,
+        default: 'primary'
+      },
+      size: {
+        required: false,
+        type: String,
+        default: 'normal'
+      },
+      to: {
+        required: false,
+        type: Object,
+        default: () => ({name: 'Home'})
+      }
+    }
+  }
+</script>
